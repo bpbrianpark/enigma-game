65b6b82d62ddd961c889e60d4396cb4175f6d0be
Brian Park <brianpark1208@gmail.com>
2025-09-06 00:28:05 -0700

    lazy load prisma

--- core/lib/prisma.ts
+++ core/lib/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+let prisma: PrismaClient;
 
-export const prisma = 
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query']
-    })
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  globalThis.prisma ??= new PrismaClient();
+  prisma = globalThis.prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma
+export { prisma };
